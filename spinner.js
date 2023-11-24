fb27a103ca039fe859f196aeb5b8628329389948
SerialEditor <refservicesamara@yandex.ru>
2023-11-24 17:14:37 +0300

    Удаляет вывод в консоль

--- spinner.js
+++ spinner.js
@@ -4,6 +4,5 @@ let spinnerBox = document.querySelector(".spinner-container");
 let spinner = spinnerBox.querySelector(".spinner");
 
 spinnerBox.addEventListener("click", () => {
-  console.info("Click!");
   spinner.classList.toggle("stop-animate");
 });
